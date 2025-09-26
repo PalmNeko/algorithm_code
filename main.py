b54445923bf51bbfbd2e47c5b13ebf00af8b12b4
PalmNeko <nitunamoni13579@gmail.com>
2025-09-26 15:24:54 +0900

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from typing import List, Tuple
-from local_driver import Alg3D, Board # ローカル検証用
+# from local_driver import Alg3D, Board # ローカル検証用
 import math
 import copy
-# from framework import Alg3D, Board # 本番用
+from framework import Alg3D, Board # 本番用
 
 
 class Board3d:
